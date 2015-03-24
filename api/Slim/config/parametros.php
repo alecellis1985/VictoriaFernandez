a022226f3e107a3e7f473c659c4868092f4db792
Alexander Ellis <alecellis1985@gmail.com>
2015-03-24 18:14:03 -0300

    New repository

---
+++ api/Slim/config/parametros.php
@@ -0,0 +1,10 @@
+<?php
+
+    define("CANTPAG", 8);
+    define("CANTPAGCOMMENTS", 5);
+    define("DRIVER", "mysql");
+    define("SERVIDOR", "localhost");
+    define("BASE", "angular_tutorial");
+    define("USUARIO", "root");
+    define("CLAVE", "turtleman1");
+    //define("CLAVE", "oso2203");
