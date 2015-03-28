bf00acbd1ac79f16c8b61442bf289d3f62dfa638
Agustin Daguerre <agustin.s.daguerre@gmail.com>
2015-03-27 23:14:16 -0300

    Deploy config

--- api/Slim/config/parametros.php
+++ api/Slim/config/parametros.php
@@ -1,10 +1,19 @@
 <?php
-
+if ($_SERVER['SERVER_NAME'] != "localhost") {
+    define("CANTPAG", 8);
+    define("CANTPAGCOMMENTS", 5);
+    define("DRIVER", "mysql");
+    define("SERVIDOR", "sql202.260mb.net");
+    define("BASE", "n260m_16036897_profesionalesDB");
+    define("USUARIO", "n260m_16036897");
+    define("CLAVE", "croto10");
+}else{
     define("CANTPAG", 8);
     define("CANTPAGCOMMENTS", 5);
     define("DRIVER", "mysql");
     define("SERVIDOR", "localhost");
     define("BASE", "angular_tutorial");
     define("USUARIO", "root");
-    define("CLAVE", "turtleman1");
+    define("CLAVE", "oso2203");
     //define("CLAVE", "oso2203");
+}
