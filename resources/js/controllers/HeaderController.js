d3477a3251b72200a58e0959ae191f894f49ea49
Alexander Ellis <alecellis1985@gmail.com>
2015-04-05 18:22:41 -0300

    more updates bich

--- resources/js/controllers/HeaderController.js
+++ resources/js/controllers/HeaderController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-Professionals.controller('HeaderController', ['$scope', '$routeParams', '$http', '$rootScope', '$location', function ($scope, $routeParams, $http, $rootScope, $location) {
+Professionals.controller('HeaderController', ['$scope', '$routeParams', '$http', '$rootScope', '$location','$modal', function ($scope, $routeParams, $http, $rootScope, $location,$modal) {
     
         
     $scope.template = {
@@ -8,6 +8,27 @@ Professionals.controller('HeaderController', ['$scope', '$routeParams', '$http',
         //        "about": "aboutus.html",
         //        "contact": "contactus.html"
     };
+    
+    
+    $scope.items = ['item1', 'item2', 'item3'];
+
+    $scope.ingresarAction = function (size) {
+      var modalInstance = $modal.open({
+        templateUrl: 'login.html',
+        controller: 'loginController',
+        size: size,
+        resolve: {
+          items: function () {
+            return $scope.items;
+          }
+        }
+      });
+      modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
     
     //$scope.profesionalesList = ["Abogados","Alambradores","Albañiles","Animadores","Arquitectos","Carpinteros","Constructores","Contadores","Corredores de seguro","Decoradores y Diseñadores de Interiores","Desarrolladores Web","Despachantes de aduana","Diseñadores gráficos","Economistas","Economistas","Electricistas","Escribanos","Estilistas/Esteticistas","Fisioterapeutas","Fonoaudiólogos","Herreros","Ingenieros","Médicos","Nutricionistas","Odontólogos","Pintores","Podólogos","Psicólogos","Sanitarios","Tapiceros","Veterinarios","Vidrieros"];
 //    $scope.profesionalesList = [{ name:"Seleccione Categoria",id:-1},{ name:"Abogados",id:0},{ name:"Alambradores",id:1},{ name:"Albañiles",id:2},{ name:"Animadores",id:3},{ name:"Arquitectos",id:4},{ name:"Carpinteros",id:5},{ name:"Constructores",id:6},{ name:"Contadores",id:7},{ name:"Corredores de seguro",id:8},{ name:"Decoradores y Diseñadores de Interiores",id:9},{ name:"Desarrolladores Web",id:10},{ name:"Despachantes de aduana",id:11},{ name:"Diseñadores gráficos",id:12},{ name:"Economistas",id:13},{ name:"Economistas",id:14},{ name:"Electricistas",id:15},{ name:"Escribanos",id:16},{ name:"Estilistas/Esteticistas",id:17},{ name:"Fisioterapeutas",id:18},{ name:"Fonoaudiólogos",id:19},{ name:"Herreros",id:20},{ name:"Ingenieros",id:21},{ name:"Médicos",id:22},{ name:"Nutricionistas",id:23},{ name:"Odontólogos",id:24},{ name:"Pintores",id:25},{ name:"Podólogos",id:26},{ name:"Psicólogos",id:27},{ name:"Sanitarios",id:28},{ name:"Tapiceros",id:29},{ name:"Veterinarios",id:30},{ name:"Vidrieros",id:31}];
---
+++ resources/js/controllers/LoginController.js
@@ -0,0 +1,7 @@
+'use strict';
+
+Professionals.controller('LoginController', ['$scope', '$routeParams', '$http', '$rootScope', '$location', function ($scope, $routeParams, $http, $rootScope, $location) {
+    
+    
+
+}]);
