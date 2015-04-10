c902ac6d80748f200493a2777f651d60d7311f08
Alexander Ellis <alecellis1985@gmail.com>
2015-04-10 01:10:31 -0300

    new sht

--- resources/js/directives/UserDetailsDirective.js
+++ resources/js/directives/UserDetailsDirective.js
@@ -6,7 +6,17 @@ Professionals.directive('userDetails', function(){
             selectedUser: '=',
             elemColapsed:'='
         },
-        replace: true
+        replace: true,
+        link:function($scope,element,attr){
+            
+            $scope.tabs = [
+                { title:'Servicios', content:'Dynamic content 1' },
+                { title:'Informacion', content:'Dynamic content 2', disabled: true },
+                { title:'Promociones', content:'Dynamic content 2', disabled: true },
+                { title:'Formas de pago', content:'Dynamic content 2', disabled: true },
+                { title:'Mapa', content:'Dynamic content 2', disabled: true }
+              ];
+        }
     };
 });
 
