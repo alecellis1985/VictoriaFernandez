df88515edff7dd18005ff4f23260c3a49fbbf563
Alexander Ellis <alecellis1985@gmail.com>
2015-04-28 00:51:30 -0300

    Client changes

--- resources/js/app.js
+++ resources/js/app.js
@@ -38,26 +38,8 @@ Professionals.config(function ($routeProvider, $httpProvider) { //, $provide
         templateUrl: 'resources/tpl/contacto.html',
         controller: 'ContactoController'
     });
-
-//    $routeProvider.otherwise({
-//        templateUrl: 'resources/tpl/lists.html',
-//        controller: 'HomeController'
-//    });
-
 });
 
 Professionals.run(function ($rootScope, $http, $location, $timeout, $filter) {
-    //TODO: DELETE THIS
-    $timeout(function () {
-        $('div[align="center"]').remove();
-        $('body').children().last().remove();
-    }, 1500);
+  
 });
-
-//Professionals.directive('hello', function(){
-//    return {
-//        restrict: 'E',
-//        templateUrl: 'resources/tpl/header.html',
-//        
-//    };
-//});
