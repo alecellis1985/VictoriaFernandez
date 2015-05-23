110eca3b5ca9fdf318b8a02e21e42c5fabcba914
Alexander Ellis <alecellis1985@gmail.com>
2015-05-23 01:15:28 -0300

    planes nuevo, change of categories data, add new plans to db script TODO add this to user register

--- professionalesDb.sql
+++ professionalesDb.sql
@@ -69,7 +69,67 @@ CREATE TABLE `categorias` (
 
 LOCK TABLES `categorias` WRITE;
 /*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
-INSERT INTO `categorias` VALUES (1,'Abogados'),(2,'Alambradores'),(3,'Albañiles'),(4,'Animadores'),(5,'Arquitectos'),(6,'Carpinteros'),(7,'Constructores'),(8,'Contadores'),(9,'Corredores de seguro'),(10,'Decoradores y Diseñadores de Interiores'),(11,'Desarrolladores Web'),(12,'Despachantes de aduana'),(13,'Diseñadores gráficos'),(14,'Economistas'),(15,'Electricistas'),(16,'Escribanos'),(17,'Estilistas/Esteticistas'),(18,'Fisioterapeutas'),(19,'Fonoaudiólogos'),(20,'Herreros'),(21,'Ingenieros'),(22,'Médicos'),(23,'Nutricionistas'),(24,'Odontólogos'),(25,'Pintores'),(26,'Podólogos'),(27,'Psicólogos'),(28,'Sanitarios'),(29,'Tapiceros'),(30,'Veterinarios'),(31,'Vidrieros');
+INSERT 	INTO `categorias` VALUES (1,"Abogados");
+INSERT 	INTO `categorias` VALUES (2,"Agentes Inmobiliarios");
+INSERT 	INTO `categorias` VALUES (3,"Adm. de Propiedades");
+INSERT 	INTO `categorias` VALUES (4,"Agrimensores");
+INSERT 	INTO `categorias` VALUES (5,"Arquitectos");
+INSERT 	INTO `categorias` VALUES (6,"Biólogos");
+INSERT 	INTO `categorias` VALUES (7,"Bioquímicos");
+INSERT 	INTO `categorias` VALUES (8,"Contadores");
+INSERT 	INTO `categorias` VALUES (9,"Corredores de Bolsa");
+INSERT 	INTO `categorias` VALUES (10,"Corredores de Seguros");
+INSERT 	INTO `categorias` VALUES (11,"Despachantes de Aduana");
+INSERT 	INTO `categorias` VALUES (12,"Diseñadores Gráficos/Web");
+INSERT 	INTO `categorias` VALUES (13,"Diseñadores de Interiores/Decoradores");
+INSERT 	INTO `categorias` VALUES (14,"Diseñadores Indumentaria");
+INSERT 	INTO `categorias` VALUES (15,"Diseñadores Industriales");
+INSERT 	INTO `categorias` VALUES (16,"Escribanos");
+INSERT 	INTO `categorias` VALUES (17,"Fotógrafos");
+INSERT 	INTO `categorias` VALUES (18,"Gestores");
+INSERT 	INTO `categorias` VALUES (19,"Ingeniero Agrónomo");
+INSERT 	INTO `categorias` VALUES (20,"Ingenieros Civiles");
+INSERT 	INTO `categorias` VALUES (21,"Ingenieros Eléctricos");
+INSERT 	INTO `categorias` VALUES (22,"Ingenieros en Sistemas");
+INSERT 	INTO `categorias` VALUES (23,"Ingenieros Industriales-Mecánicos");
+INSERT 	INTO `categorias` VALUES (24,"Kinesiólogos");
+INSERT 	INTO `categorias` VALUES (25,"Lic. en Administración");
+INSERT 	INTO `categorias` VALUES (26,"Lic. en Comunicación");
+INSERT 	INTO `categorias` VALUES (27,"Lic. en Comercio Exterior");
+INSERT 	INTO `categorias` VALUES (28,"Lic. en Economía");
+INSERT 	INTO `categorias` VALUES (29,"Lic. en Marketing");
+INSERT 	INTO `categorias` VALUES (30,"Lic. en Nutrición");
+INSERT 	INTO `categorias` VALUES (31,"Lic. en Publicidad");
+INSERT 	INTO `categorias` VALUES (32,"Lic. en Sistemas");
+INSERT 	INTO `categorias` VALUES (33,"Lic. en Turismo");
+INSERT 	INTO `categorias` VALUES (34,"Locutores");
+INSERT 	INTO `categorias` VALUES (35,"Médico Alergista");
+INSERT 	INTO `categorias` VALUES (36,"Médicos Cardiólogos");
+INSERT 	INTO `categorias` VALUES (37,"Médicos Cirujanos");
+INSERT 	INTO `categorias` VALUES (38,"Médicos Clínicos");
+INSERT 	INTO `categorias` VALUES (39,"Médico Deportólogo");
+INSERT 	INTO `categorias` VALUES (40,"Médicos Dermatólogos");
+INSERT 	INTO `categorias` VALUES (41,"Médicos Esp/ C. Plástica");
+INSERT 	INTO `categorias` VALUES (42,"Médicos Esp/ Fertilidad");
+INSERT 	INTO `categorias` VALUES (43,"Médico Fisioterapeuta");
+INSERT 	INTO `categorias` VALUES (44,"Médico Fonoaudiólogo");
+INSERT 	INTO `categorias` VALUES (45,"Médicos Gastroenterólogos");
+INSERT 	INTO `categorias` VALUES (46,"Médicos Neurólogos");
+INSERT 	INTO `categorias` VALUES (47,"Médicos Oftalmólogos");
+INSERT 	INTO `categorias` VALUES (48,"Médicos Radiólogos");
+INSERT 	INTO `categorias` VALUES (49,"Músicos");
+INSERT 	INTO `categorias` VALUES (50,"Odontólogos");
+INSERT 	INTO `categorias` VALUES (51,"Profesores de Inglés");
+INSERT 	INTO `categorias` VALUES (52,"Profesores de Español");
+INSERT 	INTO `categorias` VALUES (53,"Profesores de Portugués");
+INSERT 	INTO `categorias` VALUES (54,"Profesores de Chino");
+INSERT 	INTO `categorias` VALUES (55,"Psicólogos");
+INSERT 	INTO `categorias` VALUES (56,"Psicopedagogos");
+INSERT 	INTO `categorias` VALUES (57,"Químicos");
+INSERT 	INTO `categorias` VALUES (58,"Rematadores");
+INSERT 	INTO `categorias` VALUES (59,"Técnico Prevensionista");
+INSERT 	INTO `categorias` VALUES (60,"Traductores");
+INSERT 	INTO `categorias` VALUES (61,"Veterinarios");
 /*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -162,6 +222,37 @@ INSERT INTO `formasdepago` VALUES (1,20,0,0,0,0),(2,21,1,1,1,1),(3,22,0,0,0,0),(
 /*!40000 ALTER TABLE `formasdepago` ENABLE KEYS */;
 UNLOCK TABLES;
 
+-- 
+-- Table structure for table `plan`
+-- 
+DROP TABLE IF EXISTS `plan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `plan` (
+  `IdPlan` int(11) NOT NULL auto_increment,
+  `Tipo` varchar(25) NOT NULL,
+  `Categoria` varchar(25) NOT NULL,
+  `Precio` int(11) NOT NULL,
+  `DuracionPlan` varchar(15) NOT NULL,
+  PRIMARY KEY  (`IdPlan`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+
+-- 
+-- Dumping data for table `plan`
+-- 
+
+INSERT INTO `plan` VALUES (1, 'Profesional', 'Basico', 500, 'Mensual');
+INSERT INTO `plan` VALUES (2, 'Profesional', 'Premium', 900, 'Mensual');
+INSERT INTO `plan` VALUES (3, 'Profesional', ' Basico', 2550, 'Semestral');
+INSERT INTO `plan` VALUES (4, 'Profesional', ' Basico', 4800, 'Anual');
+INSERT INTO `plan` VALUES (5, 'Profesional', ' Premium', 4590, 'Semestral');
+INSERT INTO `plan` VALUES (6, 'Profesional', ' Premium', 8640, 'Anual');
+INSERT INTO `plan` VALUES (7, 'Empresarial', 'Basico', 800, 'Mensual');
+INSERT INTO `plan` VALUES (8, 'Empresarial', 'Premium', 1200, 'Mensual');
+INSERT INTO `plan` VALUES (9, 'Empresarial', ' Basico', 4080, 'Semestral');
+INSERT INTO `plan` VALUES (10, 'Empresarial', ' Basico', 7680, 'Anual');
+INSERT INTO `plan` VALUES (11, 'Empresarial', ' Premium', 6120, 'Semestral');
+INSERT INTO `plan` VALUES (12, 'Empresarial', ' Premium', 11520, 'Anual');
 --
 -- Table structure for table `mapa`
 --
@@ -207,6 +298,7 @@ CREATE TABLE `users` (
   `departamento` int(3) NOT NULL,
   `categoria` int(3) NOT NULL,
   `barrio` int(3) default NULL,
+  `plan` int(3) NOT NULL,
   `sitioWeb` varchar(100) NOT NULL,
   `imagenUrl` varchar(100) NOT NULL,
   `facebookUrl` varchar(250) default NULL,
@@ -227,6 +319,8 @@ CREATE TABLE `users` (
   KEY `users_barrios_idx` (`barrio`),
   KEY `users_departamento_idx` (`departamento`),
   KEY `users_categoria_idx` (`categoria`),
+  KEY `users_plan_idx` (`plan`),
+  CONSTRAINT `users_plan` FOREIGN KEY (`plan`) REFERENCES `plan` (`IdPlan`) ON UPDATE CASCADE,
   CONSTRAINT `users_categoria` FOREIGN KEY (`categoria`) REFERENCES `categorias` (`categoriaId`) ON UPDATE CASCADE,
   CONSTRAINT `users_barrios` FOREIGN KEY (`barrio`) REFERENCES `barrios` (`barrioId`) ON UPDATE CASCADE,
   CONSTRAINT `users_departamento` FOREIGN KEY (`departamento`) REFERENCES `departamentos` (`idDepartamento`) ON UPDATE CASCADE
@@ -236,12 +330,12 @@ CREATE TABLE `users` (
 --
 -- Dumping data for table `users`
 --
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (12,'Alec','Ellis','alecellis1985@gmail.com','26013794','09865321','tajes 7530','303030303',2,1,NULL,'www.alec.com','logoJPG.jpg','https://www.facebook.com/alec.ellis.714','https://www.twitter.com','https://www.linkedin.com/profile/view?id=172168721&trk=nav_responsive_tab_profile','Brindo el mejor service que puedas ver en tu vida contratame ya!!!','afsaf','asfas','fasf','fasf','fasf','fasf','fasf','Palomo','15efd829b89e9eb2287901e77c2aae1d'),(13,'Sam','Gusto','elgroso@gmail.com','26013794','09865321','tajes 7530','303030303',2,1,NULL,'www.alec.com','logoJPG.jpg','https://www.facebook.com/alec.ellis.714','https://www.twitter.com','https://www.linkedin.com/profile/view?id=172168721&trk=nav_responsive_tab_profile','Por 3 mangos te doy clase las 24 horas, te llevo el carrito de panchos y todo lo que quieras a la hora que quieras. Se bailar y cantar pero no canto a','fasf','afsaf','asfas','fasf','fasf','fasf','fasf','El_Triste','pwpwpw'),(14,'Tomy','Potatoue','los_pi@gmail.com','26013794','09865321','tajes 7530','303030303',2,1,NULL,'www.alec.com','logoJPG.jpg','https://www.facebook.com/alec.ellis.714','https://www.twitter.com','https://www.linkedin.com/profile/view?id=172168721&trk=nav_responsive_tab_profile','afsaf','afsaf','asfas','fasf','fasf','fasf','fasf','fasf','Gomon','pwpwpw'),(15,'Charli','Gut','matoluzen@gmail.com','26013794','09865321','tajes 7530','303030303',2,1,NULL,'www.alec.com','logoJPG.jpg','https://www.facebook.com/alec.ellis.714','https://www.twitter.com','https://www.linkedin.com/profile/view?id=172168721&trk=nav_responsive_tab_profile','afsaf','afsaf','asfas','fasf','fasf','fasf','fasf','fasf','Alexander','pwpwpw'),(16,'Juan','Gonzales','elgonchi@gmail.com','262321651','09861651','tucuman 2121','6251651651',2,1,NULL,'wawa.com.uy','logoJPG.jpg','https://www.facebook.com/alec.ellis.714','https://www.twitter.com','https://www.linkedin.com/profile/view?id=172168721&trk=nav_responsive_tab_profile','afsaf','asfa','fasfas','asfaf','fasf','fasf','fasf','fasf','El_Gonchi','ahfahf'),(17,'Terry','Fierry','el_garca@gmail.com','262321651','09861651','tucuman 2121','6251651651',2,1,NULL,'wawa.com.uy','logoJPG.jpg','','https://www.twitter.com','https://www.linkedin.com/profile/view?id=172168721&trk=nav_responsive_tab_profile','afsaf','asfa','fasfas','asfaf','fasf','fasf','fasf','fasf','El_Tierri','zukulu'),(18,'BinAD','Ladennw','alecellis1985@gmail.com','26013794','098635923','Maximo tajen 3565','26013794',2,1,NULL,'','','','','','qcpdEXwvoSTaqqSxKkBFQPSxrjJcLhpUgOoZFRKuOsOIpXzzuw','vxlvwXZOKZKLrUlcuusu','NXJPWGYJzUNZDACXkgDo','trSfWKgWkXIYJtLElBsA','LSyjbXeRmkYDwaCVIAsq','djYguRtulZwpstZDYzow','zMYvRrVKXzKAPWGrsxAJ','fasf','BspcijE','a8f5f167f44f4964e6c998dee827110c'),(19,'Binkg','LadeneP','alecellis1985@gmail.com','26013794','098635923','Maximo tajen 3565','26013794',2,2,NULL,'','','','','https://www.linkedin.com/profile/view?id=172168721&trk=nav_responsive_tab_profile','iJvKNLEmUFpfARQGjWxPjQFhReYRPLeUTnyeHezyVDYboFClpO','gmhtcJLGGEBMlFBgRfVK','UQoQoMAlhQxoWVrCpAyI','qIBMfQfbGqpXbvXFqrKs','piKfAawYfRCuzdcTJOhK','XpXiUaAzxCCdyFlflNfW','larZYrIPksrMTDKXSLSD','fasf','AJFuihD','a8f5f167f44f4964e6c998dee827110c'),(20,'BinWY','LadenMk','alecellis1985@gmail.com','26013794','098635923','Maximo tajen 3565','26013794',3,2,NULL,'','','','','','','','','','','','','','ehNDzsb','a8f5f167f44f4964e6c998dee827110c'),(21,'BinQa','LadenQw','alecellis1985@gmail.com','26013794','098635923','Maximo tajen 3565','26013794',2,2,NULL,'','','','','','','','','','','','','','JmddKRN','a8f5f167f44f4964e6c998dee827110c'),(22,'BinPL','LadenzG','alecellis1985@gmail.com','26013794','098635923','Maximo tajen 3565','26013794',2,2,NULL,'','','','','','','','','','','','','','aIlkhYo','a8f5f167f44f4964e6c998dee827110c'),(23,'BinWH','LadendD','alecellis1985@gmail.com','26013794','098635923','Maximo tajen 3565','26013794',2,2,NULL,'','','','','','','','','','','','','','AypaNRD','a8f5f167f44f4964e6c998dee827110c'),(24,'Bincw','LadentX','alecellis1985@gmail.com','26013794','098635923','Maximo tajen 3565','26013794',2,2,NULL,'','','','','','','','','','','','','','sdPSmOi','a8f5f167f44f4964e6c998dee827110c'),(25,'Bingd','LadenPf','alecellis1985@gmail.com','26013794','098635923','Maximo tajen 3565','26013794',2,2,NULL,'','','','','','','','','','','','','','yAvwMxn','a8f5f167f44f4964e6c998dee827110c');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
+-- Should add plan to every user to use this...
+-- LOCK TABLES `users` WRITE;
+-- /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+-- INSERT INTO `users` VALUES (12,'Alec','Ellis','alecellis1985@gmail.com','26013794','09865321','tajes 7530','303030303',2,1,NULL,'www.alec.com','logoJPG.jpg','https://www.facebook.com/alec.ellis.714','https://www.twitter.com','https://www.linkedin.com/profile/view?id=172168721&trk=nav_responsive_tab_profile','Brindo el mejor service que puedas ver en tu vida contratame ya!!!','afsaf','asfas','fasf','fasf','fasf','fasf','fasf','Palomo','15efd829b89e9eb2287901e77c2aae1d'),(13,'Sam','Gusto','elgroso@gmail.com','26013794','09865321','tajes 7530','303030303',2,1,NULL,'www.alec.com','logoJPG.jpg','https://www.facebook.com/alec.ellis.714','https://www.twitter.com','https://www.linkedin.com/profile/view?id=172168721&trk=nav_responsive_tab_profile','Por 3 mangos te doy clase las 24 horas, te llevo el carrito de panchos y todo lo que quieras a la hora que quieras. Se bailar y cantar pero no canto a','fasf','afsaf','asfas','fasf','fasf','fasf','fasf','El_Triste','pwpwpw'),(14,'Tomy','Potatoue','los_pi@gmail.com','26013794','09865321','tajes 7530','303030303',2,1,NULL,'www.alec.com','logoJPG.jpg','https://www.facebook.com/alec.ellis.714','https://www.twitter.com','https://www.linkedin.com/profile/view?id=172168721&trk=nav_responsive_tab_profile','afsaf','afsaf','asfas','fasf','fasf','fasf','fasf','fasf','Gomon','pwpwpw'),(15,'Charli','Gut','matoluzen@gmail.com','26013794','09865321','tajes 7530','303030303',2,1,NULL,'www.alec.com','logoJPG.jpg','https://www.facebook.com/alec.ellis.714','https://www.twitter.com','https://www.linkedin.com/profile/view?id=172168721&trk=nav_responsive_tab_profile','afsaf','afsaf','asfas','fasf','fasf','fasf','fasf','fasf','Alexander','pwpwpw'),(16,'Juan','Gonzales','elgonchi@gmail.com','262321651','09861651','tucuman 2121','6251651651',2,1,NULL,'wawa.com.uy','logoJPG.jpg','https://www.facebook.com/alec.ellis.714','https://www.twitter.com','https://www.linkedin.com/profile/view?id=172168721&trk=nav_responsive_tab_profile','afsaf','asfa','fasfas','asfaf','fasf','fasf','fasf','fasf','El_Gonchi','ahfahf'),(17,'Terry','Fierry','el_garca@gmail.com','262321651','09861651','tucuman 2121','6251651651',2,1,NULL,'wawa.com.uy','logoJPG.jpg','','https://www.twitter.com','https://www.linkedin.com/profile/view?id=172168721&trk=nav_responsive_tab_profile','afsaf','asfa','fasfas','asfaf','fasf','fasf','fasf','fasf','El_Tierri','zukulu'),(18,'BinAD','Ladennw','alecellis1985@gmail.com','26013794','098635923','Maximo tajen 3565','26013794',2,1,NULL,'','','','','','qcpdEXwvoSTaqqSxKkBFQPSxrjJcLhpUgOoZFRKuOsOIpXzzuw','vxlvwXZOKZKLrUlcuusu','NXJPWGYJzUNZDACXkgDo','trSfWKgWkXIYJtLElBsA','LSyjbXeRmkYDwaCVIAsq','djYguRtulZwpstZDYzow','zMYvRrVKXzKAPWGrsxAJ','fasf','BspcijE','a8f5f167f44f4964e6c998dee827110c'),(19,'Binkg','LadeneP','alecellis1985@gmail.com','26013794','098635923','Maximo tajen 3565','26013794',2,2,NULL,'','','','','https://www.linkedin.com/profile/view?id=172168721&trk=nav_responsive_tab_profile','iJvKNLEmUFpfARQGjWxPjQFhReYRPLeUTnyeHezyVDYboFClpO','gmhtcJLGGEBMlFBgRfVK','UQoQoMAlhQxoWVrCpAyI','qIBMfQfbGqpXbvXFqrKs','piKfAawYfRCuzdcTJOhK','XpXiUaAzxCCdyFlflNfW','larZYrIPksrMTDKXSLSD','fasf','AJFuihD','a8f5f167f44f4964e6c998dee827110c'),(20,'BinWY','LadenMk','alecellis1985@gmail.com','26013794','098635923','Maximo tajen 3565','26013794',3,2,NULL,'','','','','','','','','','','','','','ehNDzsb','a8f5f167f44f4964e6c998dee827110c'),(21,'BinQa','LadenQw','alecellis1985@gmail.com','26013794','098635923','Maximo tajen 3565','26013794',2,2,NULL,'','','','','','','','','','','','','','JmddKRN','a8f5f167f44f4964e6c998dee827110c'),(22,'BinPL','LadenzG','alecellis1985@gmail.com','26013794','098635923','Maximo tajen 3565','26013794',2,2,NULL,'','','','','','','','','','','','','','aIlkhYo','a8f5f167f44f4964e6c998dee827110c'),(23,'BinWH','LadendD','alecellis1985@gmail.com','26013794','098635923','Maximo tajen 3565','26013794',2,2,NULL,'','','','','','','','','','','','','','AypaNRD','a8f5f167f44f4964e6c998dee827110c'),(24,'Bincw','LadentX','alecellis1985@gmail.com','26013794','098635923','Maximo tajen 3565','26013794',2,2,NULL,'','','','','','','','','','','','','','sdPSmOi','a8f5f167f44f4964e6c998dee827110c'),(25,'Bingd','LadenPf','alecellis1985@gmail.com','26013794','098635923','Maximo tajen 3565','26013794',2,2,NULL,'','','','','','','','','','','','','','yAvwMxn','a8f5f167f44f4964e6c998dee827110c');
+-- /*!40000 ALTER TABLE `users` ENABLE KEYS */;
+-- UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
