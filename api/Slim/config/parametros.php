43fdd8727e9788be4709e55d7366e9690c4be391
Alexander Ellis <alecellis1985@gmail.com>
2015-05-27 01:15:37 -0300

    deploy params

--- api/Slim/config/parametros.php
+++ api/Slim/config/parametros.php
@@ -4,11 +4,11 @@
     define("DRIVER", "mysql");
     define("SERVIDOR", "localhost");
     define("BASE", "profesionales");
-    define("USUARIO", "root");
-    define("CLAVE", "turtleman1");
+//    define("USUARIO", "root");
+//    define("CLAVE", "turtleman1");
 //    define("CLAVE", "oso2203");
    
 //    Deeploi settin
-//    define("USUARIO", "hawkman");
-//    define("CLAVE", "zung2aban3alun4_A2n12ga");
+    define("USUARIO", "hawkman");
+    define("CLAVE", "zung2aban3alun4_A2n12ga");
    
