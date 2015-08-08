c847949b78b87bfaf84ad89d7b70219b1daef46d
Alexander Ellis <alecellis1985@gmail.com>
2015-08-07 21:33:29 -0300

    register update

--- resources/js/directives/UserDetailsDirective.js
+++ resources/js/directives/UserDetailsDirective.js
@@ -10,7 +10,7 @@ Professionals.directive('userDetails', function(){
         link:function($scope,element,attr){
             $scope.tabs = [
                 { title:'Servicios', content:'resources/tpl/tabServicios.html' },
-                { title:'Informacion', content:'resources/tpl/tabInformacion.html'},
+                { title:'Información', content:'resources/tpl/tabInformacion.html'},
                 //{ title:'Promociones', content:'resources/tpl/tabPromociones.html'},
                 { title:'Formas de pago', content:'resources/tpl/tabFormadePago.html'},
                 { title:'Mapa', content:'resources/tpl/tabMap.html'}
