ec6261b9978b20dc5cd7d667e0c0363af244eebd
Alexander Ellis <alecellis1985@gmail.com>
2015-08-21 22:46:34 -0300

    fixes de estilos y logica

--- resources/js/controllers/ProfessionalsSearchController.js
+++ resources/js/controllers/ProfessionalsSearchController.js
@@ -24,12 +24,6 @@ Professionals.controller('ProfessionalsSearchController', ['$scope', '$routePara
             $scope.selectedUser = user;
         }
 
-        $scope.sendMailToUser = function (event, user)
-        {
-            event.stopPropagation();
-            alert('sendMailToUser');
-        }
-
         //$scope.departamentosList = [{name:"Seleccione Departamento",id:-1},{name:"Montevideo",id:0},{name:"Artigas",id:1},{name:"Canelones",id:2},{name:"Cerro Largo",id:3},{name:"Colonia",id:4},{name:"Durazno",id:5},{name:"Flores",id:6},{name:"Florida",id:7},{name:"Lavalleja",id:8},{name:"Maldonado",id:9},{name:"Paysandú",id:10},{name:"Río Negro",id:11},{name:"Rivera",id:12},{name:"Rocha",id:13},{name:"Salto",id:14},{name:"San José",id:15},{name:"Soriano",id:16},{name:"Tacuarembó",id:17},{name:"Treinta y Tres",id:18}];
         $scope.departamentosList = departamentosList.data;
         $scope.departamentosList.unshift({nombreDepartamento: "Seleccione Departamento", idDepartamento: -1});
